318f1f18d8bb7136ff680d326d156f6b578edff5
Jack <hk20130120515@163.com>
2020-03-08 17:25:31 +0800

    初始化爬虫学习

---
+++ study/day01_spider/01_urllib2_urlopen.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+import urllib2
+
+# 向指定的url地址发送请求，并返回服务器响应的类文件对象
+url = "http://www.baidu.com/"
+response = urllib2.urlopen(url)
+
+# 服务器返回的类文件对象支持Python文件对象的操作方法
+# read()方法就是读取文件里的全部内容，返回字符串
+html = response.read()
+
+print(html)
--- study/day06/01_斐波纳契数列.py
+++
@@ -1,9 +0,0 @@
-# 斐波纳契数列
-a = 0
-b = 1
-i = 0
-while i < 20:
-    print(b, end="\t")
-    a = b
-    b = a + b
-    i += 1
